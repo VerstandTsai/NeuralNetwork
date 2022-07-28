c7ee5b11c89e587c5d158fd0d0227d12b3ec7ee1
VerstandTsai <hackertsaigenz@gmail.com>
2022-07-28 09:06:18 +0800

    Added a timer in the MNIST test

--- mnist.py
+++ mnist.py
@@ -1,6 +1,7 @@
 import nn
 from mnist_loader import LoadMNIST
 import numpy as np
+from time import time
 
 if __name__ == '__main__':
     load_size = 10000
@@ -16,18 +17,25 @@ if __name__ == '__main__':
         net_end = np.zeros((10, 1))
         net_end[label][0] = 1
         train_labels.append(net_end)
+
     network = nn.Model([
         nn.layers.Flatten((28, 28)),
         nn.layers.FullyConnected(16, nn.Activation('sigmoid')),
         nn.layers.FullyConnected(16, nn.Activation('sigmoid')),
         nn.layers.FullyConnected(10, nn.Activation('sigmoid')),
     ])
+
     rights = []
     for i in range(train_size, load_size):
         rights.append(1 if np.argmax(network.predict(train_images[i])) == train_labels_temp[i] else 0)
     print('Before training:')
     print(f'Accuracy: {np.average(rights) * 100}%')
+
+    start = time()
     network.fit(train_images[:train_size], train_labels[:train_size], 1, 5, plot_losses=True)
+    end = time()
+    print(f'Time used: {end - start} seconds.')
+
     rights = []
     for i in range(train_size, load_size):
         rights.append(1 if np.argmax(network.predict(train_images[i])) == train_labels_temp[i] else 0)
